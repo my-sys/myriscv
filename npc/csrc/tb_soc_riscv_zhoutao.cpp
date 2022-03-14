11a8e43e19b89b883ae865d96f1b49058fb54364
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 10:19:09 +0800

    >  sim RTL ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:19:09 up 3 min,  1 user,  load average: 0.51, 0.38, 0.17

--- npc/csrc/tb_soc_riscv_zhoutao.cpp
+++ npc/csrc/tb_soc_riscv_zhoutao.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <stdio.h>
 #include <stdlib.h>
 //#include <assert.h>
@@ -5,22 +6,17 @@
 #include <verilated_vcd_c.h>
 #include "Vsoc_riscv_zhoutao_top.h"
 #include <nvboard.h>
-#define MAX_RISCV_SIM_TIME 2000
+#define MAX_RISCV_SIM_TIME 20000
 #define IS_WAVE_OR_NVBOARD 0
 
 
 # if IS_WAVE_OR_NVBOARD 
-
+#include <pthread.h>      
 static vluint64_t riscv_sim_time = 0;
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
 
-
-class Incentive_source{
-    public:
-        bool 
-}
-
+static uint64_t map_pin[100];
 
 static Vsoc_riscv_zhoutao_top* top;
 
@@ -46,10 +42,95 @@ void sim_exit(){
     delete contextp;
 }
 
+void reset(int n){
+    top->reset = 0;
+    top->clk = 0;
+    step_and_dump_wave();
+    top->clk = 1;
+    step_and_dump_wave();
+    while(n--){
+        top->clk = 0;
+        step_and_dump_wave();
+        top->clk = 1;
+        step_and_dump_wave();
+    }
+    top->reset = 0;
+    top->clk = 0;
+    step_and_dump_wave();
+    top->clk = 1;
+    step_and_dump_wave();
+
+}
+
+void delay_clk(int n){
+    uint64_t temp_count = riscv_sim_time;
+    while((riscv_sim_time - temp_count < n)&& (riscv_sim_time <MAX_RISCV_SIM_TIME));
+}
+
+void kbd_sencode(uint8_t code){
+    bool send_buffer[11];
+    send_buffer[0] = 0;
+    for(int j = 0 ; j<8; j++){
+        send_buffer[j] = (code>>j) & 1;
+    }
+    send_buffer[9] = ~(^code);
+    send_buffer[10] = 1;
+    for(int i = 0; i<11;i++){
+        map_pin[1] = send_buffer[i];
+        delay_clk(3);
+        map_pin[0] = 0;
+        delay_clk(3);
+        map_pin[0] = 1;
+    }
+}
+
+void* test_bench(){
+    // 设置激励信号的值
+   // while(riscv_sim_time < MAX_RISCV_SIM_TIME){
+        
+   // }
+
+   delay_clk(20);
+   kbd_sencode(0x1c);
+   delay_clk(20);
+   kbd_sencode(0xf0);
+   delay_clk(20);
+   kbd_sencode(0x1c);
+   delay_clk(40);
+   kbd_sencode(0x1B);
+   delay_clk(20);
+   kbd_sencode(0x1B);
+   delay_clk(20);
+   kbd_sencode(0xF0);
+   kbd_sencode(0x1B);
+   delay_clk(20);
+
+}
+
+void update_pin(){
+    // 更新激励信号的值
+    top->ps2_clk = map_pin[0];
+    top->ps2_data = map_pin[1];
+}
+
 int main(int argc, char** argv,char** cnv){
     sim_init();
+    reset();
     
-    top->clk=0b0;step_and_dump_wave();
+    pthread_t tid = 1;
+    int ret = pthread_create(&tid, NULL, test_bench, NULL);
+    while(riscv_sim_time < MAX_RISCV_SIM_TIME){
+        update_pin();
+        top->clk = 0;
+        step_and_dump_wave();
+        top->clk = 1;
+        step_and_dump_wave();
+        riscv_sim_time ++;
+    } 
+    pthread_exit(NULL);
+    sim_exit();
+    
+    /*top->clk=0b0;step_and_dump_wave();
     int i =0;
     while(i<= 1000){
         top->clk=~top->clk;
@@ -59,7 +140,7 @@ int main(int argc, char** argv,char** cnv){
         }
         step_and_dump_wave();
     }    
-    sim_exit();
+    sim_exit();*/
 
    /* VerilatedContext* contextp = new VerilatedContext;
     Verilated::commandArgs(argc, argv);
