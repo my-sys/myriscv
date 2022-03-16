88342893beafa82e1c432ddcf7ceae1398f181dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-16 18:52:51 +0800

    >  sim RTL ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:52:51 up  8:40,  1 user,  load average: 1.20, 1.14, 0.92

--- npc/build/obj_dir/Vsoc_riscv_zhoutao_top___024root.cpp
+++ npc/build/obj_dir/Vsoc_riscv_zhoutao_top___024root.cpp
@@ -48,30 +48,23 @@ VL_INLINE_OPT void Vsoc_riscv_zhoutao_top___024root___sequent__TOP__2(Vsoc_riscv
     vlSelf->valid = ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__h_valid) 
                      & (IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid));
     vlSelf->soc_riscv_zhoutao_top__DOT__vga_data = 
-        ((0x61a80U >= (0x7ffffU & (((((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
-                                       ? (0x3ffU & 
-                                          ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
-                                           - (IData)(0x24U)))
-                                       : 0U) << 7U) 
-                                    + (((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
-                                         ? (0x3ffU 
-                                            & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
-                                               - (IData)(0x24U)))
-                                         : 0U) << 9U)) 
-                                   + ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__h_valid)
-                                       ? (0x3ffU & 
-                                          ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__x_cnt) 
-                                           - (IData)(0x91U)))
-                                       : 0U)))) ? vlSelf->soc_riscv_zhoutao_top__DOT__vga_mem1__DOT__vga_mem
-         [(0x7ffffU & (((((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
-                           ? (0x3ffU & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
-                                        - (IData)(0x24U)))
-                           : 0U) << 7U) + (((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
-                                             ? (0x3ffU 
-                                                & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
-                                                   - (IData)(0x24U)))
-                                             : 0U) 
-                                           << 9U)) 
+        ((0x61a80U >= (0x7ffffU & (((IData)(0x280U) 
+                                    * ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
+                                        ? (0x3ffU & 
+                                           ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
+                                            - (IData)(0x24U)))
+                                        : 0U)) + ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__h_valid)
+                                                   ? 
+                                                  (0x3ffU 
+                                                   & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__x_cnt) 
+                                                      - (IData)(0x91U)))
+                                                   : 0U))))
+          ? vlSelf->soc_riscv_zhoutao_top__DOT__vga_mem1__DOT__vga_mem
+         [(0x7ffffU & (((IData)(0x280U) * ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__v_valid)
+                                            ? (0x3ffU 
+                                               & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__y_cnt) 
+                                                  - (IData)(0x24U)))
+                                            : 0U)) 
                        + ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__h_valid)
                            ? (0x3ffU & ((IData)(vlSelf->soc_riscv_zhoutao_top__DOT__vga_ctrl1__DOT__x_cnt) 
                                         - (IData)(0x91U)))
