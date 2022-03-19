5e6ad57cc7dcc11c54c89059ee270473cd9c0084
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-19 20:58:27 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:58:27 up  8:37,  1 user,  load average: 0.08, 0.07, 0.01

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[]) {
  // return is_exit_status_bad();
  //
  char c[1000] = {};
- strcpy(c,"((  700* 221/ 778*194))");
+ /*strcpy(c,"((  700* 221/ 778*194))");
  bool success = true;
  init_regex();
  word_t temp_num = expr(c,&success);
  if(success){
      printf("%lu \n",temp_num);
- }
-/* FILE *fptr;
+ }*/
+ FILE *fptr;
  if((fptr = fopen("./tools/gen-expr/input","r"))==NULL){
      printf("Error! can not open the file");
      return -1;
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
      }
      memset(c,'\0',sizeof(c));
  }
- if(fptr!=NULL){fclose(fptr);}*/
+ if(fptr!=NULL){fclose(fptr);}
 
  return 0;
 }
