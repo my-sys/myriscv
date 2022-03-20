15c59fdcf81f11260a4d1de5e58587bb8bf8e932
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 16:41:33 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:41:33 up  4:40,  1 user,  load average: 0.12, 0.10, 0.06

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -20,5 +20,17 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+    for(int i = 0; i<32; i++){
+        if(strcmp(s,regs[i])==0){
+            *success = true;
+            return gpr(i);        
+        };
+    }
+    int temp = atoi(s);
+    if(temp>0 && temp<=31){
+        *success = true;
+        return gpr(temp);
+    }
+    *success = false;
+    return 0;
 }
--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -5,10 +5,11 @@
  */
 #include <sys/types.h>
 #include <regex.h>
-
+#include <memory/paddr.h>
 enum {
   TK_NOTYPE = 256, TK_EQ,
-  TK_NUM,
+  TK_NUM,TK_HEX_NUM,TK_REG,
+  TK_DEREF,TK_NEQ,TK_AND,
   /* TODO: Add more token types */
 
 };
@@ -31,6 +32,10 @@ static struct rule {
  // {"\\\+", '+'},         // plus
   {"\\+",'+'}, 
   {"==", TK_EQ},        // equal
+  {"!=", TK_NEQ},
+  {"&&", TK_AND},
+  {"0x[0-9]+",TK_HEX_NUM},
+  {"(?<=\\$)[a-z]?([0-9]|[1-9][0-9])",TK_REG},
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -75,8 +80,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-      //  Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-      //      i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
@@ -95,6 +100,11 @@ static bool make_token(char *e) {
           case TK_NUM:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_NUM;nr_token++;break;
           case TK_NOTYPE:break;
           case TK_EQ:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_EQ;nr_token++;break;
+          case TK_NEQ:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_NEQ;nr_token++;break;
+          case TK_AND:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_AND;nr_token++;break;
+          case TK_HEX_NUM:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_HEX_NUM;nr_token++;break;
+          case TK_REG:strncpy(tokens[nr_token].str,substr_start,31);tokens[nr_token].str[31]='\0';tokens[nr_token].type = TK_REG;nr_token++;break; 
+
           default: assert(0);
         }
         break;
@@ -129,18 +139,39 @@ uint32_t find_poistion_main_operator(uint32_t p , uint32_t q){
     int main_operator_type = 0;
     int count = 0;
     uint32_t position = 0;
-    while(p < q){
-        if(tokens[p].type == '+' || tokens[p].type == '-'){
+    while(p < q){ //the effectiveness of this method is very , it must exist better method.
+        if(tokens[p].type == TK_AND){
             if(count == 0 ){
                 main_operator_type = tokens[p].type;
                 position = p;
+            }   
+        }else if(tokens[p].type == TK_EQ || tokens[p].type == TK_NEQ){
+            if(count == 0){
+                if(main_operator_type != TK_AND){
+                    main_operator_type = tokens[p].type;
+                    position = p;
+                }
+            }
+        }else if(tokens[p].type == '+' || tokens[p].type == '-'){
+            if(count == 0 ){
+                if(main_operator_type != TK_AND && main_operator_type != TK_EQ && main_operator_type != TK_NEQ){
+
+                    main_operator_type = tokens[p].type;
+                    position = p;
+                }
             }
         }else if(tokens[p].type == '*' || tokens[p].type == '/'){
-            if(count == 0 && main_operator_type != '-'&& main_operator_type != '+'){
+            if(count == 0 && main_operator_type != TK_AND && main_operator_type != TK_EQ && main_operator_type != TK_NEQ  && main_operator_type != '-'&& main_operator_type != '+'){
                 main_operator_type = tokens[p].type;
                 position = p;
             }
-        }else if(tokens[p].type == '('){
+        }else if(tokens[p].type == TK_DEREF){
+            if(count == 0 && main_operator_type != '*' && main_operator_type != '/' && main_operator_type != TK_AND && main_operator_type != TK_EQ && main_operator_type != TK_NEQ  && main_operator_type != '-'&& main_operator_type != '+'){
+                main_operator_type = tokens[p].type;
+                position = p;
+            }
+        }
+        else if(tokens[p].type == '('){
             count ++;
         }else if(tokens[p].type == ')'){
             count --;
@@ -156,8 +187,25 @@ word_t eval(uint32_t p, uint32_t q){
         return 0;
     }
     else if (p == q){
-
-        return atol(tokens[p].str);
+        if(tokens[p].type == TK_NUM){
+            return atol(tokens[p].str);
+        }else if(tokens[p].type == TK_HEX_NUM){
+            return strtoul(tokens[p].str, NULL, 16); 
+        }else if(tokens[p].type == TK_REG){
+            bool success = false;
+            word_t temp =  isa_reg_str2val(tokens[p].str,&success);
+            if(success == false){
+                assert(0);
+                return 0;
+            }else{
+                return temp;
+            }
+        }
+        else{
+            assert(0);
+            return 0;
+        }
+        
     }
     else if (check_parentheses(p,q) == true){
         return eval(p + 1, q - 1);
@@ -173,6 +221,10 @@ word_t eval(uint32_t p, uint32_t q){
             case '-': return val1 - val2;
             case '*': return val1 * val2;
             case '/': return val1 / val2;
+            case TK_EQ: return (val1 == val2)? 1:0;
+            case TK_NEQ: return (val1 != val2)? 1:0;
+            case TK_AND: return (val1 && val2);
+            case TK_DEREF: return paddr_read(val2, 4);
             default: assert(0);
         }
 
@@ -187,6 +239,11 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
+  for(int i = 0; i < nr_token; i++){
+      if(tokens[i].type == '*' && (i == 0 || (tokens[i-1].type != TK_NUM && tokens[i-1].type != TK_HEX_NUM && tokens[i-1].type != ')'))){
+          tokens[i].type = TK_DEREF;
+      }
+  }
   return eval(0,nr_token - 1);
   //return 0;
 }
