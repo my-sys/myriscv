245717e6863b5af57d41098a1d29a701370f870e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 21:06:39 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:06:39 up  9:53,  1 user,  load average: 0.21, 0.11, 0.03

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,7 +23,7 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-static word_t immJ(uint32_t i) { return SEXT(BITS(i,31,31) << 19 | BITS(i,30,21) | BITS(i,20,20)<<10 | BITS(i,19,12),20)<<1;}
+static word_t immJ(uint32_t i) { return SEXT(BITS(i,31,31) << 19 | BITS(i,30,21) | BITS(i,20,20)<<10 | BITS(i,19,12)<<11,20)<<1;}
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
