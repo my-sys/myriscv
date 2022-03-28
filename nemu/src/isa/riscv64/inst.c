df7be516716791c2765f0ea12b6541565802be6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 22:15:40 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:15:40 up  1:10,  1 user,  load average: 0.12, 0.08, 0.01

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -24,6 +24,7 @@ static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
 static word_t immJ(uint32_t i) { return SEXT(BITS(i,31,31) << 19 | BITS(i,30,21) | BITS(i,20,20)<<10 | BITS(i,19,12)<<11,20)<<1;}
+static word_t immB(uint32_t i) { return SEXT(BITS(i,31,31)<<11 | BITS(i,30,25)<<4 | BITS(i,11,8) | BITS(i,10,10)<<10,12)<<1; }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -35,7 +36,9 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
-    case TYPE_J: src1I(immJ(i)); printf("src1 0x%lx \n",*src1);break;
+    case TYPE_J: src1I(immJ(i)); break;
+    case TYPE_R: src1R(rs1); src2R(rs2);break;
+    case TYPE_B: src1R(rs1); src2R(rs2);destI(immB(i));break;
   }
 }
 
@@ -53,7 +56,12 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(dest) = src1 );
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
-
+  INSTPAT("0000000 00000 00000 000 00000 01100 11", add    , R, R(dest) = src1 + src2);
+  INSTPAT("??????? ????? ????? 000 ????? 00110 11", addiw  , I, R(dest) = SEXT(BITS(src1,31,0) + BITS(src2,31,0),32));
+  INSTPAT("0000000 ????? ????? 000 ????? 01110 11", addw   , R, R(dest) = SEXT(BITS(src1,31,0)+ BITS(src2,31,0),32));
+  INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , R, R(dest) = src1 & src2);
+  INSTPAT("??????? ????? ????? 111 ????? 00100 11", andi   , I, R(dest) = src1 & src2);
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, (src1 == src2) ?(s->dnpc = s->pc + dest):(s->dnpc = s->snpc));
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
   
