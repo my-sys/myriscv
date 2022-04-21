f580ed3e2da2dde6a146a09434643a68aeeccbac
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 15:59:57 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:59:57 up  6:14,  1 user,  load average: 0.21, 0.22, 0.14

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -1,6 +1,7 @@
 #ifndef __CPU_IFETCH_H__
 
 #include <memory/vaddr.h>
+
 static int xingk_inst_num = 0;
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
