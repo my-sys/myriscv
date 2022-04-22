e2f68644775890a7c4aba9c0dc18cac51c169b9d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-22 17:35:47 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:35:47 up  6:11,  1 user,  load average: 0.18, 0.14, 0.10

--- abstract-machine/scripts/isa/riscv64.mk
+++ abstract-machine/scripts/isa/riscv64.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE := riscv64-linux-gnu-
 #COMMON_FLAGS  := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
 
-COMMON_FLAGS  := -fno-pic -march=rv64i -mcmodel=medany -mstrict-align -mabi=lp64
+COMMON_FLAGS  := -fno-pic -march=rv64im -mcmodel=medany -mstrict-align -mabi=lp64
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -melf64lriscv
