b1b2b72c68c4dc4f893205e47375510c198bbe06
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 22:47:58 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:47:58 up 12:46,  1 user,  load average: 0.12, 0.07, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -35,11 +35,36 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 }
 
+void xingk_iringbuf(Decode *s, vaddr_t pc){
+
+  char *p = s->logbuf;
+  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+  int ilen = s->snpc - s->pc;
+  int i;
+  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+  for (i = 0; i < ilen; i ++) {
+    p += snprintf(p, 4, " %02x", inst[i]);
+  }
+  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  int space_len = ilen_max - ilen;
+  if (space_len < 0) space_len = 0;
+  space_len = space_len * 3 + 1;
+  memset(p, ' ', space_len);
+  p += space_len;
+
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+
+  strcpy(iringbuf[g_nr_guest_inst%16], s->logbuf);
+}
+
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
+  xingk_iringbuf(s,pc);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
@@ -67,20 +92,8 @@ static void execute(uint64_t n) {
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-// iringbuf ------;
-    strcpy(iringbuf[g_nr_guest_inst%16],s.logbuf); 
     trace_and_difftest(&s, cpu.pc); 
-    if (nemu_state.state != NEMU_RUNNING) {
-        printf("tttttttttt\n");
-        if(nemu_state.state == NEMU_ABORT){ //iringbuf
-            for(int i =0;i<16;i++){
-                if((g_nr_guest_inst%16) == i){
-                    printf("%s  <----- \n",iringbuf[i]);
-                }else{
-                    printf("%s\n",iringbuf[i]);
-                }
-            }
-        } // iringbuf 
+    if (nemu_state.state != NEMU_RUNNING) { 
         break;
     }
     IFDEF(CONFIG_DEVICE, device_update());
@@ -98,6 +111,13 @@ static void statistic() {
 
 void assert_fail_msg() {
   isa_reg_display();
+  for(int i = 0; i<16; i++){
+      if((g_nr_guest_inst % 16) == i){
+          printf("%s   <------ \n",iringbuf[i]);
+      }else{
+          printf("%s\n",iringbuf[i]);
+      }
+  }
   statistic();
 }
 
