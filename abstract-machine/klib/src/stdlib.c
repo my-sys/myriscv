20ef012c68b6a6370d446bbb02cc189acc44a97f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-20 17:08:36 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:08:36 up  6:55,  1 user,  load average: 0.53, 0.59, 0.42

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 struct block *xingk_hbrk = NULL;
-static char *temp_hbrk = NULL;
+//static char *temp_hbrk = NULL;
 typedef struct block Block;
 static unsigned long int next = 1;
 
@@ -57,7 +57,7 @@ void *malloc(size_t size) {
 // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))  --xingk
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
   //panic("Not implemented");
-  /*Block *temp;
+  Block *temp;
   if(size == 0){
       return NULL;
   }
@@ -90,8 +90,8 @@ void *malloc(size_t size) {
      temp = temp->next;
 
      
-  }*/
-    if(temp_hbrk == NULL){
+  }
+   /* if(temp_hbrk == NULL){
         temp_hbrk = (void *)ROUNDUP(heap.start, 8);
     }
 
@@ -103,7 +103,7 @@ void *malloc(size_t size) {
     for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)temp_hbrk; p ++) {
         *p = 0;
     }
-    return old;
+    return old;*/
 #endif
   return NULL;
 }
