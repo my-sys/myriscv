f2c3522882e4bb300f9c951a67e5304ed2e6ca84
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 21:17:38 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:17:38 up 14:50,  1 user,  load average: 0.18, 0.21, 0.24

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -56,17 +56,29 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
+  if(vgactl_port_base[1] == 1){
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
 
+// xingk 
+static void vga_ctl_handler(uint32_t offset, int len, bool is_write) {
+  assert(offset == 0 || offset == 4);
+  if(is_write){
+    vga_update_screen();
+  }
+}
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
-  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
+  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, vga_ctl_handler);
+  //add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
 #endif
 
   vmem = new_space(screen_size());
