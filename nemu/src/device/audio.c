241baae5ad7dd211444e54d2dc758567d302bf25
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 10:02:07 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:02:07 up 11:10,  1 user,  load average: 0.50, 0.24, 0.14

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -24,6 +24,7 @@ static void audio_play(void *userdata, uint8_t *stream, int len) {
   if(index_addr + nread > CONFIG_SB_SIZE){
     char *src = (char *)sbuf + index_addr;
     uint32_t temp_count = CONFIG_SB_SIZE - index_addr;
+    printf("aa size %d,index %d\n",temp_count,index_addr);
     strncpy((char *)stream,src,temp_count);
     src = (char *)sbuf;
     strncpy((char*)stream,src+temp_count,len - temp_count);
