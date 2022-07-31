a1ec7f17582695d40764e4c47c1c27abafa4195b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 17:56:40 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:56:40 up 1 day, 15:06,  1 user,  load average: 0.59, 0.66, 0.79

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -18,7 +18,7 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
   cfg->bufsize = buf_size;//inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
