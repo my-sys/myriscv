3e21d21bc23038ce0f246a60e0638864ef87bc8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 18:01:20 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:01:20 up 1 day, 15:11,  1 user,  load average: 1.65, 1.08, 0.93

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = 400;  // TODO: get the correct width
-  int h = 300;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i< w * h; i++)fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // int i;
+  // int w = 400;  // TODO: get the correct width
+  // int h = 300;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i< w * h; i++)fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -33,10 +33,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       fb[(y+j)*400+x+i]=temp[j*w+i];
     }
   }
-  //if (ctl->sync) {
+  if (ctl->sync) {
     outl(SYNC_ADDR, 1);
-    printf("kk la\n");
-  //}
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
