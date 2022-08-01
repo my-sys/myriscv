12a2fb4b4ebbf8f484c4aeea70ccba34d0a5cff8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 15:02:41 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:02:41 up 2 days, 48 min,  1 user,  load average: 0.82, 0.36, 0.15

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,10 +9,10 @@ void init_proc(void);
 
 int main() {
   extern const char logo[];
-  printf("%s", logo);
+  printf("%s", logo);printf("halo\n");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
-  printf("halo\n");
+  
   init_mm();
 
   init_device();
