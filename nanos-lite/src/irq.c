704b05e151290f71ab74f4e9f6fa0232f996f965
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 19:40:08 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:40:08 up 2 days, 16:53,  1 user,  load average: 0.19, 0.21, 0.23

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,12 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD : printf(" yield \n");break;
+    case EVENT_YIELD : 
+    asm volatile("csrrw t1,mstatus,t2 \n\t"
+      "addi t1,t1,4\n\t"
+      "csrrw t1,mstatus,t1\n"
+    );
+    printf(" yield \n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
