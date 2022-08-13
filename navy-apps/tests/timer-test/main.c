b06316532dcafb99bd1453918e49b1752b1d0ffd
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 19:47:34 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:47:34 up 10:25,  1 user,  load average: 0.59, 0.70, 0.41

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,15 +2,16 @@
 #include <sys/time.h>
 int main(){
     struct timeval tv_begin, tv_end;
-    
+    __uint64_t temp,temp1;
     for(int i = 0; i<500;i++){
         gettimeofday(&tv_begin,NULL);
-
+        temp =  tv_begin.tv_sec*1000000 + tv_begin.tv_usec;
         do{
             
             gettimeofday(&tv_end,NULL);
+            temp1 = tv_end.tv_sec*1000000 + tv_end.tv_usec;
             printf("ha %d\n",tv_end.tv_sec);
-        }while((tv_end.tv_usec-tv_begin.tv_usec) < 500000);
+        }while((temp1-temp) < 500000);
         printf("hello1 \n");
     }
     
