a186b1c302649486a91ee2013a0fad41e9228e26
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 16:55:13 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:55:13 up 19:03,  1 user,  load average: 0.77, 0.39, 0.18

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <NDL.h>
 int main(){
-    struct timeval tv_begin, tv_end;
+    // struct timeval tv_begin, tv_end;
+    // __uint64_t temp,temp1;
+    // for(int i = 0; i<500;i++){
+    //     gettimeofday(&tv_begin,NULL);
+    //     temp =  tv_begin.tv_sec*1000000 + tv_begin.tv_usec;
+    //     do{
+            
+    //         gettimeofday(&tv_end,NULL);
+    //         temp1 = tv_end.tv_sec*1000000 + tv_end.tv_usec;
+    //         //printf("ha %d\n",temp1-temp);
+    //     }while((temp1-temp) < 500000);
+    //     printf("hello1 \n");
+    // }
+    
     __uint64_t temp,temp1;
     for(int i = 0; i<500;i++){
-        gettimeofday(&tv_begin,NULL);
-        temp =  tv_begin.tv_sec*1000000 + tv_begin.tv_usec;
+        
+        temp =  NDL_GetTicks();
         do{
             
-            gettimeofday(&tv_end,NULL);
-            temp1 = tv_end.tv_sec*1000000 + tv_end.tv_usec;
+            
+            temp1 = NDL_GetTicks();
             //printf("ha %d\n",temp1-temp);
-        }while((temp1-temp) < 500000);
+        }while((temp1-temp) < 500);
         printf("hello1 \n");
-    }
-    
+    }   
     return 0;
 }
