f1abb2a21c03a17781f7f65686fad1911de4f7ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 15:43:30 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:43:30 up 17:32,  1 user,  load average: 0.31, 0.13, 0.14

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,12 +63,13 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   //printf("_write %x,%d\n",buf,count);
-  int ret;
-  if(fd != 5){
-     ret = _syscall_(SYS_write,fd,buf,count);
-  }else{
-    ret = _syscall_(SYS_read,fd,buf,count);
-  }
+  int ret = _syscall_(SYS_write,fd,buf,count);
+  // int ret;
+  // if(fd != 5){
+  //    ret = _syscall_(SYS_write,fd,buf,count);
+  // }else{
+  //   ret = _syscall_(SYS_read,fd,buf,count);
+  // }
   
 
   //int ret = _syscall_(SYS_read,fd,buf,count);
