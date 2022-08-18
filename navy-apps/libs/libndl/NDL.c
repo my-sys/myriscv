1fd3177d1ce6daf3ec678cd617d0eac089b12284
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 15:46:40 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:46:40 up 17:35,  1 user,  load average: 0.20, 0.19, 0.17

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -49,7 +49,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   fseek(fp, temp_xy, SEEK_SET);
   size_t temp_wh = (w<<16) +h;
   printf("%x,%d,%d,%x \n",(uint32_t)pixels,w,h,temp_wh);
-  fwrite(pixels,4,800,fp);
+  fwrite(0x8000,4,800,fp);
   //write(5,pixels,800);
 }
 
