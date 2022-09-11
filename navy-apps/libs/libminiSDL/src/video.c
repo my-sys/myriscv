9a61b26b80db2a12390b4e170bf12cab673cf900
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-11 13:09:01 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:09:01 up 33 min,  1 user,  load average: 0.78, 0.89, 0.73

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -105,7 +105,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       for(int j = 0; j < w; j++){
         //color_buf[i*w+j] = *(temp + x + (y+i)* (s->w) + j);
         //printf("1 %d\n",i);
-        color_buf[i*w+j] = 0; 
+        //color_buf[i*w+j] = 0; 
         //printf("2\n");
       }
     } 
