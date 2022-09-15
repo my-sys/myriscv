622abc002247d197a5d17e74aff66aedf6c6fda1
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 13:21:16 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:21:16 up  1:22,  1 user,  load average: 0.81, 0.88, 1.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,8 +27,8 @@ void init_proc() {
   // load program here
   //naive_uload(NULL, "/bin/bmp-test");
   //naive_uload(NULL,"/bin/nterm");
-  //naive_uload(NULL, "/bin/nslider");
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/nslider");
+  //naive_uload(NULL, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
