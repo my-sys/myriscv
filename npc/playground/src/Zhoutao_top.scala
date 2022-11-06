da86da0e218010f13671e24b8be56f6675ad3a0b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 11:01:46 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:01:46 up 12 min,  1 user,  load average: 0.35, 0.50, 0.34

--- npc/playground/src/Zhoutao_top.scala
+++ npc/playground/src/Zhoutao_top.scala
@@ -1,7 +1,7 @@
 import circt.stage._
 
 object Zhoutao_top extends App{
-    def top = new GCD()
+    def top = new riscv_soc()
     val useMFC = true 
     val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
     if (useMFC){
