ea41b0be2441e1c9e3dce354b8d70bbe6fcd8bfe
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 15:09:39 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:09:39 up  2:42,  1 user,  load average: 0.54, 0.38, 0.28

--- npc/playground/src/core/regsfile.scala
+++ npc/playground/src/core/regsfile.scala
@@ -3,7 +3,7 @@ import chisel3.util._
 
 class RegCtrl extends CoreParameters{
     val regfile = Mem(32, UInt(RegDataLen.W))
-    def read(addr: UInt): UInt = Mux(addr === 0.U, 0.U, regfile.read(addr))
+    def read(addr: UInt): UInt = Mux(addr === 0.U, 0.U(64.W), regfile.read(addr))
     def write(addr: UInt, data: UInt) = {
         regfile(addr) := data
     }
