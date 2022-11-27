49e05158080c7776a4ea623415dc557ffaa4155e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-27 18:12:51 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:12:51 up  5:46,  1 user,  load average: 0.09, 0.10, 0.08

--- npc/playground/src/riscv_soc.scala
+++ npc/playground/src/riscv_soc.scala
@@ -31,16 +31,16 @@ class riscv_soc extends Module{
   val core = Module(new Core)
   val axi_ram = Module(new AXI_RAM)
   val difftest_inst     = WireInit(2.U(64.W))
-  val temp_inst         = core.io.out.raddr
+  //val temp_inst         = core.io.out.raddr
   
   val difftest_reg      = VecInit(Seq.fill(32)(0.U(64.W)))
   val difftest_pc       = WireInit(0.U(64.W))
   
-  BoringUtils.addSource(temp_inst,"DIFFTEST_INST1")
+  //BoringUtils.addSource(temp_inst,"DIFFTEST_INST1")
 
   BoringUtils.addSink(difftest_reg,"DIFFTEST_REG")
   BoringUtils.addSink(difftest_pc,"DIFFTEST_PC")
-  BoringUtils.addSink(difftest_inst,"DIFFTEST_INST1")
+  BoringUtils.addSink(difftest_inst,"DIFFTEST_INST")
   io.difftest_reg     := difftest_reg
   io.difftest_pc      := difftest_pc
   io.difftest_inst    := difftest_inst
