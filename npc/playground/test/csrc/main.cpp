3bd4b92db60d58a5ffbee6942228bfe5b1753cd4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-28 11:05:53 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:05:53 up 54 min,  1 user,  load average: 0.82, 0.42, 0.25

--- npc/playground/test/csrc/main.cpp
+++ npc/playground/test/csrc/main.cpp
@@ -1,11 +1,29 @@
 #include "emulator.h"
-
+#include <getopt.h>
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *log_file = NULL;
 
 extern void init_ram(const char* img_file);
 static int parse_args(int argc, char *argv[]){
-
+    const struct option table[] = {
+        {"log"      , required_argument, NULL, 'l'},
+        {"diff"     , required_argument, NULL, 'd'},
+        {0          ,                  , NULL, 0},
+    }
+    int opt;
+    for(int i =0 ;i<argc;i++){
+      printf("i= %d, %s\n",i,argv[i]);
+    } 
+    while((opt = getopt_long(argc, argv, "-l:d:",table, NULL)) != -1){
+        switch(opt){
+            case 'l': log_file = optarg; break;
+            case 'd': diff_so_file = optarg;break;
+            case 1  : img_file = optarg;return 0;
+            default: ;
+        }
+    }
+    return 0;   
 }
 void welcome(){
     printf("^_^ ***welcome to start debug in npc ***  O(∩_∩)O~\n");
