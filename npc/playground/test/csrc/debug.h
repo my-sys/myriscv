383703679291892052db2a1789cc6df8580dbe32
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-28 15:35:29 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:35:29 up  5:24,  1 user,  load average: 5.01, 2.53, 1.32

--- npc/playground/test/csrc/debug.h
+++ npc/playground/test/csrc/debug.h
@@ -2,9 +2,6 @@
 #define __DEBUG_H__
 
 #include "common.h"
-#include <stdio.h>
-#include <utils.h>
-
 #define Log(format, ...) \
     _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
