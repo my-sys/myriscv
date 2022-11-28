a4795e0cf48f49eae912d8cd462f6bf7294f189e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-28 15:42:48 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:42:48 up  5:31,  1 user,  load average: 0.54, 1.49, 1.32

--- npc/playground/test/csrc/debug.h
+++ npc/playground/test/csrc/debug.h
@@ -6,6 +6,16 @@
     _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
+#define Assert(cond, format, ...) \
+  do { \
+    if (!(cond)) { \
+        (fflush(stdout), fprintf(stderr, ASNI_FMT(format, ASNI_FG_RED) "\n", ##  __VA_ARGS__)); \
+      extern FILE* log_fp; fflush(log_fp); \
+      extern void assert_fail_msg(); \
+      assert_fail_msg(); \
+      assert(cond); \
+    } \
+  } while (0)
 
 #define panic(format, ...) Assert(0, format, ## __VA_ARGS__)
 
