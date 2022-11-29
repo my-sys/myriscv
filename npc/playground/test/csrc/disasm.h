0976fd8641c8888c3697ee84124cbba1fc7ccff3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 16:40:36 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:40:36 up  6:03,  1 user,  load average: 0.51, 0.49, 0.49

--- npc/playground/test/csrc/disasm.h
+++ npc/playground/test/csrc/disasm.h
@@ -2,6 +2,6 @@
 #define __DISASM_H__
 #include "common.h"
 #define CONFIG_ITRACE
-void init_disasm(const char *triple);
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+extern void init_disasm(const char *triple);
+extern void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 #endif
