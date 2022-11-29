f8d1817520df7800d228b7113b4ae3087cf5eeb9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 18:59:42 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:59:42 up  8:22,  1 user,  load average: 0.33, 0.22, 0.19

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -2,11 +2,11 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 
-int isa_mmu_check(vaddr_t vaddr, int len, int type){
-  uintptr_t base = csr(0x180);
+// int isa_mmu_check(vaddr_t vaddr, int len, int type){
+//   uintptr_t base = csr(0x180);
 
-  return 1;
-}
+//   return 1;
+// }
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   return MEM_RET_FAIL;
