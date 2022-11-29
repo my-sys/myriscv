1ed8e3ae67f19b5e64ffb7d5fc7dc3fbfad16869
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 19:26:14 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:26:14 up  8:49,  1 user,  load average: 0.88, 0.71, 0.56

--- npc/playground/test/csrc/sdb.c
+++ npc/playground/test/csrc/sdb.c
@@ -22,7 +22,7 @@ static void trace_and_difftest()
     p += snprintf(p, 4, " %02x", inst[i]);
   }
   //extern void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, logbuf + sizeof(logbuf) - p, reg[0],inst,4);
+  disassemble(p, logbuf + sizeof(logbuf) - p, reg[0],(uint8_t *)&reg[1],4);
   puts(logbuf);
 #endif 
 }
