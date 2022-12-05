85f7c6aefbb140f63afec204ff6fe108765220b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-05 17:55:52 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:55:52 up  7:44,  1 user,  load average: 0.57, 0.34, 0.14

--- npc/playground/test/csrc/difftest.cpp
+++ npc/playground/test/csrc/difftest.cpp
@@ -10,7 +10,7 @@
 #define DIFFTEST_TO_REF 1
 #define DIFFTEST_TO_DUT 0
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_memcpy1)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
@@ -39,8 +39,8 @@ void init_difftest(char *ref_so_file, long img_size, int port){
 	handle = dlopen(ref_so_file, RTLD_LAZY | RTLD_DEEPBIND);
 	assert(handle);
 
-  	ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
-  	assert(ref_difftest_memcpy);
+  	ref_difftest_memcpy1 = dlsym(handle, "difftest_memcpy");
+  	assert(ref_difftest_memcpy1);
 
   	ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
   	assert(ref_difftest_regcpy);
@@ -60,7 +60,7 @@ void init_difftest(char *ref_so_file, long img_size, int port){
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
 	ref_difftest_init(port);
-  	ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+  	ref_difftest_memcpy1(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
 	cpu.pc = RESET_VECTOR;
 	cpu.gpr[0] = 0;
   	ref_difftest_regcpy(&cpu,DIFFTEST_TO_REF);
