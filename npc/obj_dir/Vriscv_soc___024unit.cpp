d8cfa8071d1d345d8ad976255f9eb4b39c159da9
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-07 21:29:54 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:29:54 up 11:12,  1 user,  load average: 0.29, 0.53, 0.34

---
+++ npc/obj_dir/Vriscv_soc___024unit.cpp
@@ -0,0 +1,30 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vriscv_soc.h for the primary calling header
+
+#include "Vriscv_soc___024unit.h"
+#include "Vriscv_soc__Syms.h"
+
+#include "verilated_dpi.h"
+
+//==========
+
+extern "C" void ramCtrl(long long raddr, long long* rdata, long long waddr, long long wdata, const svBitVecVal* wstrb, svBit wen);
+
+VL_INLINE_OPT void Vriscv_soc___024unit____Vdpiimwrap_ramCtrl_TOP____024unit(QData/*63:0*/ raddr, QData/*63:0*/ &rdata, QData/*63:0*/ waddr, QData/*63:0*/ wdata, CData/*7:0*/ wstrb, CData/*0:0*/ wen) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        Vriscv_soc___024unit____Vdpiimwrap_ramCtrl_TOP____024unit\n"); );
+    // Body
+    long long raddr__Vcvt;
+    for (size_t raddr__Vidx = 0; raddr__Vidx < 1; ++raddr__Vidx) raddr__Vcvt = raddr;
+    long long rdata__Vcvt;
+    long long waddr__Vcvt;
+    for (size_t waddr__Vidx = 0; waddr__Vidx < 1; ++waddr__Vidx) waddr__Vcvt = waddr;
+    long long wdata__Vcvt;
+    for (size_t wdata__Vidx = 0; wdata__Vidx < 1; ++wdata__Vidx) wdata__Vcvt = wdata;
+    svBitVecVal wstrb__Vcvt[1];
+    for (size_t wstrb__Vidx = 0; wstrb__Vidx < 1; ++wstrb__Vidx) VL_SET_SVBV_I(8, wstrb__Vcvt + 1 * wstrb__Vidx, wstrb);
+    svBit wen__Vcvt;
+    for (size_t wen__Vidx = 0; wen__Vidx < 1; ++wen__Vidx) wen__Vcvt = wen;
+    ramCtrl(raddr__Vcvt, &rdata__Vcvt, waddr__Vcvt, wdata__Vcvt, wstrb__Vcvt, wen__Vcvt);
+    rdata = rdata__Vcvt;
+}
