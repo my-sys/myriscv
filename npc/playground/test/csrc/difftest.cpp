6fd672f15820d2ecc7f6807aff171b8292cf10f6
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-09 20:51:41 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:51:41 up  9:47,  1 user,  load average: 0.40, 0.25, 0.15

--- npc/playground/test/csrc/difftest.cpp
+++ npc/playground/test/csrc/difftest.cpp
@@ -10,7 +10,7 @@
 #define DIFFTEST_TO_REF 1
 #define DIFFTEST_TO_DUT 0
 
-static uint64_t ref_this_pc = RESET_VECTOR
+static uint64_t ref_this_pc = RESET_VECTOR;
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
