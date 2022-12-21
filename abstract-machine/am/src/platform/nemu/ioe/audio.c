66f94a31a365dbe1f33deff047b17fdac710e240
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-21 15:18:15 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:18:15 up  1:16,  1 user,  load average: 0.95, 1.06, 0.83

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -9,51 +9,53 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-static uint32_t index_addr = 0;
-static uint32_t buf_size = 0x10000;
+// static uint32_t index_addr = 0;
+// static uint32_t buf_size = 0x10000;
 void __am_audio_init() {
 
   //buf_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = true;
-  cfg->bufsize = 0x10000;//inl(AUDIO_SBUF_SIZE_ADDR);
+//   cfg->present = true;
+//   cfg->bufsize = 0x10000;//inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, 1);
+//   outl(AUDIO_FREQ_ADDR, ctrl->freq);
+//   outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
+//   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+//   outl(AUDIO_INIT_ADDR, 1);
   
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_COUNT_ADDR);
+//   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  int len = ctl->buf.end - ctl->buf.start;
-  uint32_t count;
-  do{
-    count = inl(AUDIO_COUNT_ADDR);
-  }while((count+len)>buf_size);
-  //buf 缓冲区循环
-  if(index_addr + len > buf_size){
-    uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
-    uint32_t temp_count = buf_size - index_addr;
-    memcpy(dest,ctl->buf.start,temp_count);
-    dest = (uint8_t *)AUDIO_SBUF_ADDR;
-    memcpy(dest,ctl->buf.start+temp_count,len - temp_count);
-    index_addr = (index_addr + len)% buf_size;
-  }else{
-    uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
-    memcpy(dest,ctl->buf.start,len);
-    index_addr = index_addr + len;
-  }
-  // uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
-  // memcpy(dest,ctl->buf.start,len);
-  // index_addr = index_addr + len;
-  outl(AUDIO_COUNT_ADDR,count+len);
+//   int len = ctl->buf.end - ctl->buf.start;
+//   uint32_t count;
+//   do{
+//     count = inl(AUDIO_COUNT_ADDR);
+//   }while((count+len)>buf_size);
+//   //buf 缓冲区循环
+//   if(index_addr + len > buf_size){
+//     uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
+//     uint32_t temp_count = buf_size - index_addr;
+//     memcpy(dest,ctl->buf.start,temp_count);
+//     dest = (uint8_t *)AUDIO_SBUF_ADDR;
+//     memcpy(dest,ctl->buf.start+temp_count,len - temp_count);
+//     index_addr = (index_addr + len)% buf_size;
+//   }else{
+//     uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
+//     memcpy(dest,ctl->buf.start,len);
+//     index_addr = index_addr + len;
+//   }
+//   //------------------
+//   // uint8_t * dest = (uint8_t *)AUDIO_SBUF_ADDR + index_addr;
+//   // memcpy(dest,ctl->buf.start,len);
+//   // index_addr = index_addr + len;
+//   //--------------------------
+//   outl(AUDIO_COUNT_ADDR,count+len);
 }
