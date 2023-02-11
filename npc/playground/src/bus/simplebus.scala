b68a21f3db4aaef9ff76222b34f1dc6f32caf9b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-11 16:30:34 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:30:34 up  5:46,  1 user,  load average: 0.00, 0.10, 0.13

--- npc/playground/src/bus/simplebus.scala
+++ npc/playground/src/bus/simplebus.scala
@@ -42,8 +42,8 @@ class Crossbar extends Module{
 		val AXI_Bus		= new AXI4Bus
 	})
 	val lockFun = ((x:SimpleBus_aw) => x.isBurst())
-	val aw_arb = Module(new LockingArbiter(chiselTypeOf(SimpleBus_aw),2,2,Some(lockFun)))
-	val ar_arb = Module(new LockingArbiter(chiselTypeOf(SimpleBus_ar),2,0,false.B))
+	val aw_arb = Module(new LockingArbiter(chiselTypeOf(new SimpleBus_aw),2,2,Some(lockFun)))
+	val ar_arb = Module(new LockingArbiter(chiselTypeOf(new SimpleBus_ar),2,0,false.B))
 	aw_arb.io.in(0) <> io.ICache_bus.aw 
 	aw_arb.io.in(1) <> io.DCache_bus.aw 
 
