45b758ef0d4c53e0bdf11fef70446777a55bba55
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 20:54:23 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:54:23 up 10:01,  1 user,  load average: 0.34, 0.65, 0.45

--- npc/playground/src/core/fetch.scala
+++ npc/playground/src/core/fetch.scala
@@ -68,11 +68,12 @@ class Fetch extends Module{
 		}
 	}.otherwise{
 		reg_pc_0 	:= reg_pc_0
-		reg_pc_1    := Mux(!stall | flush,0.U,reg_pc_1)
-		reg_inst 	:= Mux(!stall | flush,0.U,reg_inst)
+		reg_pc_1    := Mux(stall,reg_pc_1,Mux(need_update,reg_temp_pc_1,0.U))
+		reg_inst 	:= Mux(stall,reg_inst,Mux(need_update,reg_temp_inst,0.U))
 		reg_next_pc := Mux(flush,next_pc,reg_next_pc)
 		reg_flush	:= Mux(flush,true.B,reg_flush)
 		reg_valid	:= Mux(stall,reg_valid,true.B)
+		need_update := Mux(stall,need_update,false.B)
 	}
 	io.bus.valid 		:= reg_valid 
 	io.bus.bits.pc_0	:= reg_pc_0
