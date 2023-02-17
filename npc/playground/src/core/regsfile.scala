6939dc0238b64a7066e72eb8115b5c5cd5045068
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 16:05:23 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:05:23 up  5:12,  1 user,  load average: 0.76, 0.26, 0.14

--- npc/playground/src/core/regsfile.scala
+++ npc/playground/src/core/regsfile.scala
@@ -3,6 +3,7 @@ import chisel3.util._
 
 class RegCtrl extends CoreParameters{
     val regfile = Mem(32, UInt(RegDataLen.W))
+	// this not obey the rules (when write and read both in one cycle),you should handle by yourself
     def read(addr: UInt): UInt = Mux(addr === 0.U, 0.U(64.W), regfile.read(addr))
     def write(addr: UInt, data: UInt) = {
         regfile(addr) := data
