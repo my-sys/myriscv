3bbf8ae8962d380ec4c45e66b374cc5b374ab338
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 13:05:46 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:05:46 up  3:30,  1 user,  load average: 0.13, 0.04, 0.01

--- npc/playground/src/core/exu/alu_exu.scala
+++ npc/playground/src/core/exu/alu_exu.scala
@@ -98,6 +98,7 @@ class ALU_EXU extends Module with CoreParameters{
     // subtraction, All data is assumed to be in the form of complement
     val sub_data1 = op_data1
     val sub_data2 = rs2_data//op_data2
+	////subresult(64)不是符号位,是进位,要分清
     val subresult = Cat(0.U(1.W),sub_data1) + (sub_data2 ^ "hffff_ffff_ffff_ffff".U) + 1.U
     
     //  unsigend rs1 < rs2   must check this flag
