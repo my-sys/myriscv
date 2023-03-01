dc0dc496e6f623b481e44989b1e1a7507c971465
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-01 11:53:49 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:53:49 up  1:07,  1 user,  load average: 0.67, 0.80, 0.70

--- npc/playground/test/csrc/ram.cpp
+++ npc/playground/test/csrc/ram.cpp
@@ -92,6 +92,7 @@ extern "C" void ramCtrl(paddr_t raddr, uint64_t *rdata, uint8_t rflag,paddr_t wa
 		}else if((0x10003000<=raddr)&(raddr <= 0x10003FFF)){ //keyboard
 			*rdata = keyboard_read(raddr - 0x10003000);
 		}else if((0x10005000<=raddr)&(raddr <= 0x2FFFFFFF)){
+			printf("raddr %lx \n",raddr);
 			*rdata = rtc_read(raddr - 0x10005000);
 		}
 	}else{
