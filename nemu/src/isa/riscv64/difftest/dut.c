ff7f3a2101aff97eb03789b3e922b5552b8d154f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 16:29:51 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:29:51 up  7:20,  1 user,  load average: 0.33, 0.23, 0.20

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -9,10 +9,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     }
     for(int i=0;i<32;i++){
         if(ref_r->gpr[i] != gpr(i)){
+            printf("ref gpr[%d]=%lx,nemu gpr[%d]=%lx\n",i,ref_r->gpr[i],i,gpr(i));
             printf("difftest false \n");
             return false;
         }
-    }    
+    }
     return true;
 }
 
