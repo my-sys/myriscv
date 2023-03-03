e645b7bdd6772caadccfea9076d0bdc42ecb79f0
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 18:57:44 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:57:44 up  9:48,  1 user,  load average: 0.20, 0.21, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -107,7 +107,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 #endif
-  //xingk_iringbuf(s,pc);
+  xingk_iringbuf(s,pc);
 }
 
 static void execute(uint64_t n) {
