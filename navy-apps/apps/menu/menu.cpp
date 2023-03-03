deab818835729911b7b6bf14bc7db74128db3d28
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 21:23:20 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:23:20 up 12:14,  1 user,  load average: 0.54, 0.56, 0.46

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -109,7 +109,9 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[2] = NULL;
       clear_display();
       SDL_UpdateRect(screen, 0, 0, 0, 0);
+	  printf("zzzz\n");
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
+	  printf("zz34zz\n");
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
       fprintf(stderr, "Choose a number between %d and %d\n\n", 0, i_max);
