3695be02cd5031a265b08c574100a3aeb28052bd
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-05 10:02:35 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:02:35 up 8 min,  1 user,  load average: 0.86, 0.57, 0.28

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -40,10 +40,10 @@ void init_proc() {
 
   // load program here
   //naive_uload(NULL, "/bin/bmp-test");
-  naive_uload(NULL,"/bin/nterm");
+  //naive_uload(NULL,"/bin/nterm");
   //naive_uload(NULL, "/bin/nslider");
   //naive_uload(NULL, "/bin/bird");
-  //naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/pal");
   //naive_uload(NULL,"/bin/fceux");
   //naive_uload(NULL,"/bin/menu");
   //naive_uload(NULL,"/bin/typing-game");
