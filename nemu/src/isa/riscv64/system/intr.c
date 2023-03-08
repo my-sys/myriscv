daabd9a01da3f076affd6603f3cd95dfc46f2c28
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 15:51:07 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:51:07 up  6:13,  1 user,  load average: 0.29, 0.30, 0.40

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -8,6 +8,22 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   csr(0x341) = epc;
   // mcause
   csr(0x342) = NO;
+#ifndef CONFIG_TARGET_SHARE
+  // mstatus
+  if(csr(0x300) & 0x8){
+	csr(0x300) = (csr(0x300) &0xffffffffffffff77)|0x1880;
+  }else{
+	csr(0x300) = (csr(0x300) &0xffffffffffffff77)|0x1800;
+  }
+  //csr(0x300) = (csr(0x300) &0xffffffffffffff77) | (Mux(reg_mstatus(3),"h1880".U(64.W),"h1800".U(64.W)))
+#else 
+  // mstatus
+  if(csr(0x300) & 0x8){
+	csr(0x300) = (csr(0x300) &0xffffffffffffff77)|0x80;
+  }else{
+	csr(0x300) = (csr(0x300) &0xffffffffffffff77);
+  }
+#endif
   // mtvec
   word_t address = csr(0x305);
   return address;
