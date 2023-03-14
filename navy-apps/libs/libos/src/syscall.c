6835b8d4752b6a4772c12de74ce45a7cae97a245
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-14 18:54:31 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:54:31 up  5:46,  1 user,  load average: 0.15, 0.13, 0.11

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -77,7 +77,8 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  __uint64_t ret = _syscall_(SYS_brk,increment,0,0);
+  
+  __uint64_t ret = _syscall_(SYS_brk,1,0,0);
   return (void *)ret;
 }
 
