898d3b6bb9bb80ad9953c7defd39bebf0d8ee566
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 17:09:14 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:09:14 up  7:40,  1 user,  load average: 1.53, 0.72, 0.38

--- npc/playground/src/ysyx_040318.scala
+++ npc/playground/src/ysyx_040318.scala
@@ -40,7 +40,7 @@ class AXI_Interface extends Bundle{
 
 class ysyx_22040318 extends Module{
 	val io = IO(new Bundle{
-		val inerrupt = Input(Bool())
+		val interrupt = Input(Bool())
 		val master 	= new AXI_Interface
 		val slave	= Flipped(new AXI_Interface)
 		val sram0 	= new SRAM_Interface
