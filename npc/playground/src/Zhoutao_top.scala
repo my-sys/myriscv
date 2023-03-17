000465fe0fedc0707d3574d67374fc501fb05bf6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 15:58:54 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:58:54 up  6:30,  1 user,  load average: 0.52, 0.49, 0.38

--- npc/playground/src/Zhoutao_top.scala
+++ npc/playground/src/Zhoutao_top.scala
@@ -4,9 +4,10 @@ import circt.stage._
 object Zhoutao_top extends App{
     def top = new ysyx_040318()	///new riscv_soc()
     val useMFC = false 
-    val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top),
-	firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix()),
-	 ModulePrefixAnnotation("ysyx_040318_"))
+    val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top)
+	// ,firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix())
+	//  ,ModulePrefixAnnotation("ysyx_040318_")
+	 )
     if (useMFC){
         (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
     } else{
