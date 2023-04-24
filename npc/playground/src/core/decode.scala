6bc568279d9455d6920b304d3778bcf7a5ac528c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-24 16:40:38 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:40:38 up  6:43,  1 user,  load average: 0.54, 0.31, 0.19

--- npc/playground/src/core/decode.scala
+++ npc/playground/src/core/decode.scala
@@ -69,10 +69,10 @@ class Decode extends Module{
 	when(ready){
 		reg_opType		:= opType
 		reg_exuType		:= exuType
-		reg_rs1_addr	:= rs1_addr
+		reg_rs1_addr	:= Mux(rs1_is_reg.asBool,rs1_addr,0.U)
 		reg_rs1_data	:= Mux(rs1_is_reg.asBool,io.normal_rd.rs1_data,0.U)
 
-		reg_rs2_addr	:= rs2_addr
+		reg_rs2_addr	:= Mux(rs2_is_reg.asBool,rs2_addr,0.U)
 		reg_rs2_data	:= Mux(rs2_is_reg.asBool,io.normal_rd.rs2_data,0.U)
 		reg_imm			:= imm_data
 		reg_pc			:= pc 
