8aa81ec8f8c7dee2e57f1abcd9c4eb9e72578a13
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 21:30:55 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:30:55 up  2:43,  1 user,  load average: 0.72, 0.36, 0.26

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -102,11 +102,11 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   //uint32_t color_buf[w*h];
   uint32_t* color_buf = (uint32_t *)malloc(w*h*(sizeof(uint32_t)));
   //printf("0x%x\n",color_buf);
-  printf("SDL_UpdateRect 1\n");
+  //printf("SDL_UpdateRect 1\n");
   if(s->format->palette == NULL){
     //printf("SDL_UpdateRect 2\n");
     uint32_t *temp = s->pixels;
-    //printf("SDL_UpdateRect 3 %d,%d\n",w,h);
+    printf("SDL_UpdateRect 3 %d,%d\n",w,h);
     //printf("s->pixels 0x%x \n",s->pixels);
     for(int i = 0; i < h; i++){
       for(int j = 0; j < w; j++){
