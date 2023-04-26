03372a9ae2ea09099c9ddd65decd540389e9ed1f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 11:00:32 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:00:32 up  1:07,  1 user,  load average: 0.80, 0.58, 0.66

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,16 +21,29 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *temp = (uint32_t *)ctl->pixels;
+//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   uint32_t *temp = (uint32_t *)ctl->pixels;
+//   int x = ctl->x;
+//   int y = ctl->y;
+//   int w = ctl->w;
+//   int h = ctl->h;
+//   int i,j;
+//   for(j=0;j<h;j++){
+//     for(i=0;i<w;i++){
+//       fb[(y+j)*400+x+i]=temp[j*w+i];
+//     }
+//   }
+
+  uint64_t *fb = (uint64_t *)(uintptr_t)FB_ADDR;
+  uint64_t *temp = (uint64_t *)ctl->pixels;
   int x = ctl->x;
   int y = ctl->y;
-  int w = ctl->w;
+  int w = (ctl->w)>>1;
   int h = ctl->h;
   int i,j;
   for(j=0;j<h;j++){
     for(i=0;i<w;i++){
-      fb[(y+j)*400+x+i]=temp[j*w+i];
+      fb[(y+j)*200+x+i]=temp[j*w+i];
     }
   }
   if (ctl->sync) {
