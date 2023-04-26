57ace3136e8e9a064ec8d5c56dbe6529d2094ae4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 12:03:46 +0800

    >  compile NEMU ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:03:46 up  2:10,  1 user,  load average: 0.82, 0.69, 0.76

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -38,7 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint64_t *temp = (uint64_t *)ctl->pixels;
   int x = 0;///((ctl->x)>>1);
   int y = ctl->y;
-  int w = 200;//(ctl->w+1)>>1;
+  int w = 50;//(ctl->w+1)>>1;
   int h = ctl->h;
   int i,j;
   for(j=0;j<h;j++){
