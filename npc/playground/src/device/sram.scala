dbe513857231ef5db0ea6b424a46576b2e7dae7b
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-29 00:25:04 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:25:04 up  1:35,  1 user,  load average: 0.45, 0.31, 0.21

--- npc/playground/src/device/sram.scala
+++ npc/playground/src/device/sram.scala
@@ -1,17 +1,17 @@
 import chisel3._ 
 import chisel3.util._ 
 
-class S011HD1P_X32Y2D128_BW extends BlackBox{
-	val io = IO(new Bundle{
-		val CLK 	= Input(Clock())
-		val Q 		= Output(UInt(128.W))
-		val CEN 	= Input(Bool())
-		val WEN 	= Input(Bool())
-		val BWEN 	= Input(UInt(128.W))
-		val A 		= Input(UInt(6.W))
-		val D 		= Input(UInt(128.W))
-	})
-}
+// class S011HD1P_X32Y2D128_BW extends BlackBox{
+// 	val io = IO(new Bundle{
+// 		val CLK 	= Input(Clock())
+// 		val Q 		= Output(UInt(128.W))
+// 		val CEN 	= Input(Bool())
+// 		val WEN 	= Input(Bool())
+// 		val BWEN 	= Input(UInt(128.W))
+// 		val A 		= Input(UInt(6.W))
+// 		val D 		= Input(UInt(128.W))
+// 	})
+// }
 
 class SRAM extends Module{
 	val io = IO(new Bundle{
@@ -22,13 +22,26 @@ class SRAM extends Module{
 		val wdata 	= Input(UInt(128.W))
 		val rdata 	= Output(UInt(128.W))
 	})
-
-	val sram = Module(new S011HD1P_X32Y2D128_BW)
-	sram.io.CLK 	:= clock 
-	sram.io.CEN 	:= io.cen
-	sram.io.WEN		:= io.wen
-	sram.io.BWEN	:= io.wmask
-	sram.io.A		:= io.addr
-	sram.io.D		:= io.wdata
-	io.rdata 		:= sram.io.Q
+	val Q    = RegInit(0.U(128.W))
+	val sram = RegInit(VecInit(Seq.fill(64)(0.U(128.W))))
+	val A = io.addr
+	val temp_data = sram(A)
+	val bwen = io.wmask
+	val D = io.wdata
+	val cen = io.cen
+	val wen = io.wen
+	
+	when(cen & wen){
+		sram(A) := (D & bwen) | (temp_data & ~bwen)
+	}
+	Q := Mux(cen && !wen,temp_data,0.U)
+	io.rdata	:= Q
+	// val sram = Module(new S011HD1P_X32Y2D128_BW)
+	// sram.io.CLK 	:= clock 
+	// sram.io.CEN 	:= io.cen
+	// sram.io.WEN		:= io.wen
+	// sram.io.BWEN	:= io.wmask
+	// sram.io.A		:= io.addr
+	// sram.io.D		:= io.wdata
+	// io.rdata 		:= sram.io.Q
 }
