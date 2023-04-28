f1b8ab0ace7ad278d00de41a013b18d3ad8feee5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-29 01:39:29 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:39:29 up  2:49,  1 user,  load average: 0.13, 0.21, 0.41

--- npc/playground/src/core/br_predictor.scala
+++ npc/playground/src/core/br_predictor.scala
@@ -29,7 +29,7 @@ class Br_predictor extends Module{
 	//pc = Tag0 +index+Tag1 
 	// 1      ()        64        2
 	//valid ----Tag------ Taget pc---- BrType 
-	val BTB = RegInit(VecInit(Seq.fill(64)(0.U(128.W)))
+	val BTB = RegInit(VecInit(Seq.fill(64)(0.U(128.W))))
 	//val PHT = Mem(64,UInt(2.W))
 	val RAS = RegInit(VecInit(Seq.fill(6)(0.U(64.W))))//Mem(6,UInt(64.W))
 	val PHT = RegInit(VecInit(Seq.fill(64)("b10".U(2.W))))
