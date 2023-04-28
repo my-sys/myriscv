74fd6d099d48aa28f331830cdf982d1fb02f6db9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-28 17:27:57 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:27:57 up  1:03,  1 user,  load average: 0.23, 0.46, 0.51

--- npc/playground/src/core/exu/mu_exu.scala
+++ npc/playground/src/core/exu/mu_exu.scala
@@ -41,8 +41,8 @@ class MUL extends Module{
 	val is_rs2_signed 	= io.exuType(0)
 
 
-	val mul_data1 := Mux(is_32,Cat(Fill(98,rs1_data(31)),rs1_data(31,0)),Mux(is_rs1_signed,Cat(Fill(66,rs1_data(63)),rs1_data),Cat(0.U(66.W),rs1_data)))
-	val mul_data2 := Mux(is_32,Cat(Fill(33,rs2_data(31)),rs2_data(31,0)),Mux(is_rs2_signed,Cat(rs2_data(63),rs2_data),Cat(0.U(1.W),rs2_data)))	
+	val mul_data1 = Mux(is_32,Cat(Fill(98,rs1_data(31)),rs1_data(31,0)),Mux(is_rs1_signed,Cat(Fill(66,rs1_data(63)),rs1_data),Cat(0.U(66.W),rs1_data)))
+	val mul_data2 = Mux(is_32,Cat(Fill(33,rs2_data(31)),rs2_data(31,0)),Mux(is_rs2_signed,Cat(rs2_data(63),rs2_data),Cat(0.U(1.W),rs2_data)))	
 
 	val temp_mul2 		= Cat(Fill(1,mul_data2(64)),Cat(mul_data2,0.U(1.W)))
 	val reg_ready = RegInit(true.B)
