b69276ecb28f98c4203aeb7c63473e0b15bd48a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-02 15:34:16 +0800

    >  generate verilog ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:34:16 up  3:52,  1 user,  load average: 0.30, 0.18, 0.19

--- npc/playground/src/core/decode.scala
+++ npc/playground/src/core/decode.scala
@@ -74,7 +74,10 @@ class Decode extends Module{
 	val temp_mem_dest  = Mux(inst(5),false.B,true.B)
 	val temp_mem_rs2   = Mux(inst(5),true.B,false.B)
 	val temp_op_is_imm = !inst(5)
-	val temp_op_itype = Mux(temp_op_is_imm,Mux((fun === ALUType.alu_sll(4,2)) | (fun === ALUType.alu_srl(4,2)),Inst_type.Type_IR,Inst_type.Type_I),Inst_type.Type_R)
+	val is_sr = fun === ALUType.alu_srl(4,2)
+	val temp_kk = Cat(int(30),fun_exuType)
+	val temp_op_exuType = Mux(temp_op_is_imm,Mux(is_sr,temp_kk,fun_exuType),temp_kk)
+	val temp_op_itype = Mux(temp_op_is_imm,Mux((fun === ALUType.alu_sll(4,2)) | is_sr),Inst_type.Type_IR,Inst_type.Type_I),Inst_type.Type_R)
 	val temp_op_rs2   = Mux(temp_op_is_imm,false.B,true.B)
 	// val temp_jal_jalr = Mux(inst(3),List(Op_type.op_bru,BRUType.bru_jal,Inst_type.Type_J,true.B,false.B,false.B),
 	// List(Op_type.op_bru,BRUType.bru_jalr,Inst_type.Type_I,true.B,true.B,false.B))
