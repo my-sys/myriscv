46c6a015449017192899734ca50a1568b1194667
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-02 23:11:05 +0800

    >  sim RTL xingk ysyx_22040318 周涛 Linux xingk-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:11:05 up 11:28,  1 user,  load average: 1.63, 0.69, 0.49

--- npc/obj_dir/Vriscv_soc___024root__DepSet_h1cc2eb16__1.cpp
+++ npc/obj_dir/Vriscv_soc___024root__DepSet_h1cc2eb16__1.cpp
@@ -57,7 +57,7 @@ void Vriscv_soc___024root___eval(Vriscv_soc___024root* vlSelf) {
 #ifdef VL_DEBUG
                 Vriscv_soc___024root___dump_triggers__ico(vlSelf);
 #endif
-                VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9686, "", "Input combinational region did not converge.");
+                VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9685, "", "Input combinational region did not converge.");
             }
             vlSelf->__VicoIterCount = ((IData)(1U) 
                                        + vlSelf->__VicoIterCount);
@@ -80,7 +80,7 @@ void Vriscv_soc___024root___eval(Vriscv_soc___024root* vlSelf) {
 #ifdef VL_DEBUG
                     Vriscv_soc___024root___dump_triggers__act(vlSelf);
 #endif
-                    VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9686, "", "Active region did not converge.");
+                    VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9685, "", "Active region did not converge.");
                 }
                 vlSelf->__VactIterCount = ((IData)(1U) 
                                            + vlSelf->__VactIterCount);
@@ -95,7 +95,7 @@ void Vriscv_soc___024root___eval(Vriscv_soc___024root* vlSelf) {
 #ifdef VL_DEBUG
                 Vriscv_soc___024root___dump_triggers__nba(vlSelf);
 #endif
-                VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9686, "", "NBA region did not converge.");
+                VL_FATAL_MT("/home/xingk/ysyx-workbench/npc/build/riscv_soc.v", 9685, "", "NBA region did not converge.");
             }
             __VnbaIterCount = ((IData)(1U) + __VnbaIterCount);
             Vriscv_soc___024root___eval_nba(vlSelf);
